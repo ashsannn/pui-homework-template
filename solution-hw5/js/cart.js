e01ab6b52c6220e7270527e8ff7766a0f419ac19
Ashley Sanchez <ashleysa@andrew.cmu.edu>
2024-10-08 20:19:17 -0400

    update 10.08 8pm panic 2.0

--- solution-hw5/js/cart.js
+++ solution-hw5/js/cart.js
@@ -1,6 +1,20 @@
-const cart = []; //CART
+/*-------------------------------------ROLL CLASS--------------------------------*/
+
+class Roll {
+    constructor(rollType, rollGlazing, packSize, basePrice) {
+        this.type = rollType;
+        this.glazing =  rollGlazing;
+        this.size = packSize;
+        this.basePrice = basePrice;
+
+        this.element = null;
+    }
+
+}
 
 /* ------------------------------ CART FUNCTIONALITY ----------------------------*/
+
+const cart = []; //CART
 
 function addRollToCart(rollType, rollGlazing, packSize, basePrice) {
     const roll = new Roll(rollType, rollGlazing, packSize, basePrice); //new roll
@@ -8,6 +22,8 @@ function addRollToCart(rollType, rollGlazing, packSize, basePrice) {
     console.log(cart); //print cart
     return roll;
 };
+
+/* ------------------------------ CART FUNCTIONALITY ----------------------------*/
 
 const rollOne = addRollToCart(
     'Original',
@@ -45,4 +61,33 @@ function createElement(roll){
     roll.element = clone.querySelector('.roll');
 
     console.log(roll.element);
+
+    const cartListElement = document.querySelector('.all-cart-items-container');
+    cartListElement.prepend(roll.element); 
+    updateElement(roll);
+}
+
+
+function updateElement(roll) {
+    const cartImageElement = roll.element.querySelector('.cart-thumbnail');
+	const cartTitleElement = roll.element.querySelector('.cart-description');
+
+    //BELOW, UPDATE DOM ELEMENTS USING ROLL PROPERTIES
+    cartImageElement.src = roll.noteImageURL;
+    cartTitleElement.innerText = roll.noteTitle;
+    cartBodyElement.innerText = roll.noteBody;
+
+    //this gets a reference to the trash can item
+    const btnDelete = notecard.element.querySelector('.icon-delete');
+   //this syntax says, when the trash can button clicked, run the 
+   //instructions in curly brackets
+    btnDelete.addEventListener('click', () => {deleteNote(notecard);});
+}
+
+
+//TO DELETE NOTECARD !! 
+function deleteNote(notecard) {
+    notecard.element.remove(); //Remove from DOM
+    notecardSet.delete(notecard); //We have to delete the data as well
+    //from the set - not just graphical representation
 }
